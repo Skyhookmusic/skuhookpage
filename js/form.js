be74a7ba08bd90aeb56cdc146345a3b57d49bfa1
Bohdan <bodjabdld@gmail.com>
2017-04-11 16:53:28 +0000

    get carusel

--- js/form.js
+++ js/form.js
@@ -1,19 +1,22 @@
-$(function() {
-    $('#btn-send').click(sendForm);
-});
-
+document.querySelector('input[type=submit]').addEventListener('click', sendForm);
 function sendForm(e) {
-    e.preventDefault();
-    $.ajax({
-        url: "https://formspree.io/bodjabdld@gmail.com",
-        method: "POST",
-        data: {
-            name: $('#client-name').val(),
-            email: $('#client-email').val(),
-            comment: $('#comment').val()
-        },
-        dataType: "json"
-    })
+  var form = document.querySelector('form');
+  if (form.checkValidity()) {
+	  e.preventDefault(); // Prevent form submit
+    // Instead of alert write code that sends valid form
+    alert($.ajax({
+                url: "https://formspree.io/alionka_g@mail.ru",
+                method: "POST",
+                data: {
+                    name: $('#client_name').val(),
+                    email: $('#client_email').val(),
 
-    
+                },
+                dataType: "json"
+            })
+            .done(function() {
+                $('form').html("Дякуємо!<br>Ми з Вами зв'яжемось у найкоротший термін");
+            });
+    });
+  }
 }
