a173151777e262ba45903dc96accc9c6de8a3e50
Bohdan <bodjabdld@gmail.com>
2017-04-12 13:41:37 +0000

    changed layout sm

--- js/form.js
+++ js/form.js
@@ -1,35 +1,6 @@
-// document.querySelector('input[type=submit]').addEventListener('click', sendForm);
-// function sendForm(e) {
-//   var form = document.querySelector('form');
-//   if (form.checkValidity()) {
-// 	  e.preventDefault(); // Prevent form submit
-//     // Instead of alert write code that sends valid form
-//     alert($.ajax({
-//                 url: "https://formspree.io/bodjabdld@gmail.com",
-//                 method: "POST",
-//                 data: {
-//                     name: $('#client_name').val(),
-//                     email: $('#client_email').val(),
-//                      comment: $('#comment').val(),
-
-//                 },
-//                 dataType: "json"
-//             })
-//             .done(function() {
-//                 $('form').html("Дякуємо!<br>Ми з Вами зв'яжемось у найкоротший термін");
-//             })
-//     );
-//   }
-// }
-
-// $('button').click(function() {
-//     location.href = "index.html";
-// });
-
 $(function() {
     $(':submit').click(sendForm);
 });
-
 function sendForm(e) {
   var form = document.querySelector('form');
   if (form.checkValidity()) {
@@ -41,10 +12,8 @@ function sendForm(e) {
                     name: $('#client_name').val(),
                     email: $('#client_email').val(),
                     comment: $('#comment').val(),
-
                 },
                 dataType: "json"
             })
-
     }
 }
--- js/index.js
+++ js/index.js
@@ -1,11 +1,3 @@
-// $('.button').click(function() {
-//     location.href = "index.html";
-// });
-
-// $('.button').click(function() {
-//     location.href = "form.html";
-// });
-
  $(function() {
     $('.smooth').on('click', function(event) {
         var target = $(this.getAttribute('href'));
